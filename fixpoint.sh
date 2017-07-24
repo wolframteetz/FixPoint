740f123c5b0c948c6359bc1cbfc89bfe568a7f60
Wolfram <wolframteetz@gmail.com>
2017-07-24 20:25:52 +0200

    New

---
+++ fixpoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+hash zip 2>/dev/null || { echo >&2 “I require zip but it’s not installed.  Aborting.”; exit 1; }
+hash ffmpeg 2>/dev/null || { echo >&2 “I require ffmpeg but it’s not installed.  Aborting.”; exit 1; }
+hash unzip 2>/dev/null || { echo >&2 “I require unzip but it’s not installed.  Aborting.”; exit 1; }
+rm -r __temp
+mkdir __temp
+cd __temp
+unzip ../$1
+find ./ppt/media -iname “*.mov” -exec ffmpeg -i {} -acodec copy -vcodec msmpeg4v2 “{}_new.mov” \; -exec rm “{}” \; -exec mv “{}_new.mov” “{}” \;
+find ./ppt/media -iname “*.avi” -exec ffmpeg -i {} -acodec copy -vcodec msmpeg4v2 “{}_new.avi” \; -exec rm “{}” \; -exec mv “{}_new.avi” “{}” \;
+find ./ppt/media -iname “*.mp4” -exec ffmpeg -i {} -acodec copy -vcodec msmpeg4v2 “{}_new.mp4” \; -exec rm “{}” \; -exec mv “{}_new.mp4” “{}” \;
+zip -r ../_$1 *
+cd ..
+rm -r __temp
+echo
+echo FixPoint completed. Written file : _$1
